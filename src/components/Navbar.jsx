9151b58a17214e65ebe497dc13af61e4c72bbb93
Linet Oliech <linetaoliech@gmail.com>
2023-11-01 02:57:50 +0300

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-import { FaBars } from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaUserPlus, FaSignInAlt, FaBars } from "react-icons/fa";
+import { SiGoogletranslate } from "react-icons/si";
 
 function Navbar() {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [language, setLanguage] = useState("English");
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -12,10 +18,10 @@ function Navbar() {
     <nav className="bg-white w-full shadow-lg sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-
           <div className="md:hidden w-full flex items-center justify-between">
-
-            <a href='/' className="text-custom-blue text-4xl font-bold ">TaskFlow</a>
+            <a href="/" className="text-custom-blue text-4xl font-bold">
+              TaskFlow
+            </a>
 
             <button
               onClick={toggleMenu}
@@ -25,15 +31,26 @@ function Navbar() {
             </button>
           </div>
 
-          <a href='/' className="hidden md:flex text-custom-blue text-4xl font-bold">TaskFlow</a>
+          <a
+            href="/"
+            className="hidden md:flex text-custom-blue text-4xl font-bold"
+          >
+            TaskFlow
+          </a>
 
           {isMenuOpen && (
             <div className="md:hidden py-4 flex flex-col">
               <div className="text-center">
-                <a href="/register" className="block text-custom-blue p-2 border-2 border-custom-blue hover:text-white hover:bg-custom-blue bg-white rounded-md hover-bg-custom-blue transition duration-300">
+                <a
+                  href="/register"
+                  className="block text-custom-blue p-2 border-2 border-custom-blue hover:text-white hover-bg-custom-blue bg-white rounded-md hover-bg-custom-blue transition duration-300"
+                >
                   Register
                 </a>
-                <a href="/login" className="block text-custom-blue p-2 border-2 border-custom-blue hover:text-white hover:bg-custom-blue bg-white rounded-md hover-bg-custom-blue transition duration-300">
+                <a
+                  href="/login"
+                  className="block text-custom-blue p-2 border-2 border-custom-blue hover:text-white hover-bg-custom-blue bg-white rounded-md hover-bg-custom-blue transition duration-300"
+                >
                   Login
                 </a>
               </div>
@@ -41,12 +58,30 @@ function Navbar() {
           )}
 
           <div className="hidden md:flex space-x-4">
-            <a href="/register" className="text-custom-blue p-2 border-2 border-custom-blue hover:text-white hover:bg-custom-blue bg-white rounded-md hover-bg-custom-blue transition duration-300">
-              Register
+            <a
+              href="/register"
+              className="flex items-center text-custom-blue p-2 border-2 border-custom-blue hover:text-white hover-bg-custom-blue bg-white rounded-md hover-bg-custom-blue transition duration-300"
+            >
+              <FaUserPlus className="mr-2" /> Register
             </a>
-            <a href="/login" className="text-custom-blue p-2 border-2 border-custom-blue hover:text-white hover:bg-custom-blue bg-white rounded-md hover-bg-custom-blue transition duration-300">
-              Login
+            <a
+              href="/login"
+              className="flex items-center text-custom-blue p-2 border-2 border-custom-blue hover:text-white hover-bg-custom-blue bg-white rounded-md hover-bg-custom-blue transition duration-300"
+            >
+              <FaSignInAlt className="mr-2"/> Login
             </a>
+          </div>
+
+          <div className="mt-4 md:mt-0 flex items-center text-custom-blue  space-x-2">
+            <select
+              value={language}
+              onChange={handleLanguageChange}
+              className="text-custom-blue border-2 p-2 border-custom-blue  bg-white rounded-md  m-2 transition duration-300"
+            >
+              <option value="English">English</option>
+              <option value="French">French</option>
+            </select>
+            <SiGoogletranslate className="text-4xl" />
           </div>
         </div>
       </div>
