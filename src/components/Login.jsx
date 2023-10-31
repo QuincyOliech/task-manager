6ff02223919fadd00a8250bd477ea4dfa55476de
Linet Oliech <linetaoliech@gmail.com>
2023-11-01 02:02:40 +0300

    Update Login and register background image and styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,48 +1,97 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import backgroundImage from '../assets/images/background4.avif';
 
-const Login = () => (
-  <div className="container mx-auto px-4 py-10">
-    <h1 className="text-2xl font-semibold text-center mb-6">Login</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
+const Login = () => {
+  const notify = () => {
+    toast.success('Login successful!', {
+      position: 'top-right',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  return (
+    <div
+      className="fixed z-50 inset-0 bg-opacity-75"
+      style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backdropFilter: 'blur(8px)' }}
     >
-      {({ isSubmitting }) => (
-        <Form className="max-w-md mx-auto">
-          <div className="mb-4">
-            <label htmlFor="email" className="block font-semibold mb-2">Email</label>
-            <Field type="email" name="email" className="border rounded w-full py-2 px-3" />
-            <ErrorMessage name="email" component="div" className="text-red-500" />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block font-semibold mb-2">Password</label>
-            <Field type="password" name="password" className="border rounded w-full py-2 px-3" />
-            <ErrorMessage name="password" component="div" className="text-red-500" />
-          </div>
-          <button type="submit" disabled={isSubmitting} className="bg-blue-500 text-white font-semibold py-2 px-4 rounded w-full hover:bg-blue-700">
-            Login
-          </button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+      <div className=" min-h-screen flex flex-col items-center justify-center mx-auto px-4 py-10 ">
+      <div className="container h-100 w-96 bg-white bg-opacity-40 rounded-2xl shadow-2xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm ">
 
-export default Login;  
+        <h1 className="text-4xl text-white font-semibold text-center mb-6">Login</h1>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              notify();
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="max-w-md mx-auto p-4">
+              <div className="mb-4">
+                <label htmlFor="email" className="block mb-2 text-white">
+                  Email
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  className="border rounded w-full py-2 px-3"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block mb-2 text-white">
+                  Password
+                </label>
+                <Field
+                  type="password"
+                  name="password"
+                  className="border rounded w-full py-2 px-3"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-500"
+                />
+              </div>
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="bg-custom-blue text-white font-semibold py-2 px-4 rounded w-full hover:bg-blue-500"
+              >
+                Login
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,76 +2,88 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import backgroundImage from '../assets/images/background4.avif';
 
 const Register = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="container mx-auto px-4 py-10">
-      <h1 className="text-2xl font-semibold text-center mb-6">Register</h1>
-      <Formik
-        initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
-        validate={values => {
-          const errors = {};
-          if (!values.firstName) {
-            errors.firstName = 'Required';
-          }
-          if (!values.lastName) {
-            errors.lastName = 'Required';
-          }
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          if (!values.password) {
-            errors.password = 'Required';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            navigate('/login');
-          }, 400);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className="max-w-md mx-auto">
-            <div className="mb-4 flex justify-between">
-              <div className="w-1/2 mr-2">
-                <label htmlFor="firstName" className="block font-semibold mb-2">First Name</label>
+    <div
+      className="fixed z-50 inset-0 bg-opacity-75"
+      style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backdropFilter: 'blur(8px)' }}
+    >
+      <div className=" min-h-screen flex flex-col items-center justify-center mx-auto px-4 py-10 ">
+      <div className="container h-100 w-96 bg-white bg-opacity-40 rounded-2xl shadow-2xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm ">
+
+        <h1 className="text-4xl text-white font-semibold text-center mb-6">Create An Account</h1>
+        <h2 className="text-center m-6 text-white">
+                Already registered? <Link to="/login" className="text-custom-blue hover:underline">Go to Login</Link>
+              </h2>
+        <Formik
+          initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.firstName) {
+              errors.firstName = 'Required';
+            }
+            if (!values.lastName) {
+              errors.lastName = 'Required';
+            }
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+              errors.password = 'Required';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+              navigate('/login');
+            }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="max-w-md mx-auto p-4">
+              <div className="mb-4">
+                <label htmlFor="firstName" className="block mb-2 text-white">First Name</label>
                 <Field type="text" name="firstName" className="border rounded w-full py-2 px-3" />
                 <ErrorMessage name="firstName" component="div" className="text-red-500" />
               </div>
-              <div className="w-1/2 ml-2">
-                <label htmlFor="lastName" className="block font-semibold mb-2">Last Name</label>
+              <div className="mb-4">
+                <label htmlFor="lastName" className="block mb-2 text-white">Last Name</label>
                 <Field type="text" name="lastName" className="border rounded w-full py-2 px-3" />
                 <ErrorMessage name="lastName" component="div" className="text-red-500" />
               </div>
-            </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="block font-semibold mb-2">Email</label>
-              <Field type="email" name="email" className="border rounded w-full py-2 px-3" />
-              <ErrorMessage name="email" component="div" className="text-red-500" />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block font-semibold mb-2">Password</label>
-              <Field type="password" name="password" className="border rounded w-full py-2 px-3" />
-              <ErrorMessage name="password" component="div" className="text-red-500" />
-            </div>
-            <button type="submit" disabled={isSubmitting} className="bg-custom-blue text-white font-semibold py-2 px-4 rounded w-full hover:bg-blue-700">
-              Register
-            </button>
-            <h2 className="text-center mt-4">
-              Already registered? <Link to="/login" className="text-custom-blue hover:underline">Go to Login</Link>
-            </h2>
-          </Form>
-        )}
-      </Formik>
+              <div className="mb-4">
+                <label htmlFor="email" className="block mb-2 text-white">Email</label>
+                <Field type="email" name="email" className="border rounded w-full py-2 px-3" />
+                <ErrorMessage name="email" component="div" className="text-red-500" />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block mb-2 text-white">Password</label>
+                <Field type="password" name="password" className="border rounded w-full py-2 px-3" />
+                <ErrorMessage name="password" component="div" className="text-red-500" />
+              </div>
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="bg-custom-blue text-white font-semibold py-2 px-4 rounded w-full hover:bg-blue-500"
+              >
+                Register
+              </button>
+
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
     </div>
   );
 };
