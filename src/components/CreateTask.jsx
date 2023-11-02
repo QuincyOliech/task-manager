bd15536dca6b4d7313381eecae8961718f9c96fd
Linet Oliech <112978080+QuincyOliech@users.noreply.github.com>
2023-11-02 17:01:29 +0300

    Separate concern to new file CreateTask.jsx

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,65 +1,45 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// const CreateTask = ({ addTask }) => {
-//   const [newTask, setNewTask] = useState("");
+const CreateTask = ({addTask}) => {
+  const [task, setTask] = useState("");
 
-//   const handleAddTask = () => {
-//     if (newTask.trim() !== "") {
-//       addTask(newTask);
-//       setNewTask("");
-//     }
-//   };
+  const handleAddTask = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://65437f5c01b5e279de20773a.mockapi.io/api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: task,
+          completed: false,
+        }),
+      });
 
-//   return (
-//     <div className="flex flex-col items-center mt-8 sm:flex-row sm:justify-center">
-//       <input
-//         type="text"
-//         value={newTask}
-//         onChange={(e) => setNewTask(e.target.value)}
-//         className="border border-gray-300 rounded-md p-2 w-full sm:w-64"
-//         placeholder="Add a new task"
-//       />
-//       <button
-//         onClick={handleAddTask}
-//         className="bg-custom-blue ml-4 hover:bg-blue-500 text-white py-2 px-4 rounded mt-2 sm:mt-0"
-//       >
-//         Add Task
-//       </button>
-//     </div>
-//   );
-// };
+      const data = await response.json();
+      console.log("Task added:", data);
+      addTask(data);
+      setTask("");
+    } catch (error) {
+      console.error("Error adding task:", error);
+    }
+  };
 
-// export default CreateTask;
+  return (
+    <div className="flex flex-col items-center mt-8 sm:flex-row sm:justify-center">
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        className="border border-gray-300 rounded-md p-2 w-full sm:w-64"
+        placeholder="Add a new task"
+      />
+      <button onClick={handleAddTask} className="bg-custom-blue hover:bg-blue-500 ml-4 m-4 text-white py-2 px-4 rounded mt-2 sm:mt-0">
+        Add Task
+      </button>
+    </div>
+  );
+};
 
-// import React, { useState } from "react";
-
-// const CreateTask = ({ addTask }) => {
-//   const [newTask, setNewTask] = useState("");
-
-//   const handleAddTask = () => {
-//     if (newTask.trim() !== "") {
-//       addTask(newTask);
-//       setNewTask("");
-//     }
-//   };
-
-//   return (
-//     <div className="flex flex-col items-center mt-8 sm:flex-row sm:justify-center">
-//       <input
-//         type="text"
-//         value={newTask}
-//         onChange={(e) => setNewTask(e.target.value)}
-//         className="border border-gray-300 rounded-md p-2 w-full sm:w-64"
-//         placeholder="Add a new task"
-//       />
-//       <button
-//         onClick={handleAddTask}
-//         className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mt-2 sm:mt-0"
-//       >
-//         Add Task
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default CreateTask;
+export default CreateTask;
